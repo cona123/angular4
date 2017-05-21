7e91fdb07fd921578d448bd05892d1d3f03a9734
cona123 <chaoyue19890625@gmail.com>
2017-05-21 10:39:39 +0800

    add bootstrap4 and setting

--- freelance-camp-fe/src/app/documents/documents.component.ts
+++ freelance-camp-fe/src/app/documents/documents.component.ts
@@ -14,21 +14,21 @@ export class DocumentsComponent {//implements OnInit {
       description: 'dsssdsd',
       file_url: 'http://google.com',
       updated_at:'11/11/17',
-      image_url:'http://google.com'
+      image_url:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1495275430562&di=0a07dd08589fa9f358753136c0310652&imgtype=0&src=http%3A%2F%2Fimage.tianjimedia.com%2FuploadImages%2F2011%2F293%2F2FC145978D5W.jpg'
     },
     {
       title: 'my first doc',
       description: 'dsssdsd',
       file_url: 'http://google.com',
       updated_at:'11/11/17',
-      image_url:'http://google.com'
+      image_url:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1495275430562&di=0a07dd08589fa9f358753136c0310652&imgtype=0&src=http%3A%2F%2Fimage.tianjimedia.com%2FuploadImages%2F2011%2F293%2F2FC145978D5W.jpg'
     },
     {
       title: 'my first doc',
       description: 'dsssdsd',
       file_url: 'http://google.com',
       updated_at:'11/11/17',
-      image_url:'http://google.com'
+      image_url:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1495275430562&di=0a07dd08589fa9f358753136c0310652&imgtype=0&src=http%3A%2F%2Fimage.tianjimedia.com%2FuploadImages%2F2011%2F293%2F2FC145978D5W.jpg'
     }
   ];
   //constructor() { }
